35585ae4df0e6aa02574277135e4cc1c85994c43
Marcus <sabaledo27@hotmail.com>
2025-08-28 10:47:04 -0300

    Divide the screen into 4 quadrants

--- app/index.tsx
+++ app/index.tsx
@@ -3,17 +3,56 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Hello, World!</Text>
-      <Text>This is my app, spinning friends.</Text>
+      <View style={[styles.childContainer, styles.blue]}>
+        <Text>1</Text>
+      </View>
+
+      <View style={[styles.childContainer, styles.orange]}>
+        <Text>2</Text>
+      </View>
+
+      <View style={[styles.childContainer, styles.yellow]}>
+        <Text>3</Text>
+      </View>
+
+      <View style={[styles.childContainer, styles.red]}>
+        <Text>4</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "dodgerblue",
+    display: "flex",
+    flexWrap: "wrap",
+    height: "100%",
+    flexDirection: "column",
+    alignItems: "stretch",
+    justifyContent: "space-between",
+  },
+
+  childContainer: {
+    display: "flex",
+    minWidth: "50%",
+    minHeight: "50%",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  red: {
+    backgroundColor: "#ff0045",
+  },
+
+  blue: {
+    backgroundColor: "#90dadb",
+  },
+
+  yellow: {
+    backgroundColor: "#ffd800",
+  },
+
+  orange: {
+    backgroundColor: "#fb8500",
   },
 });
